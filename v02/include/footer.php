32769bfd0460dadf9e0b2b7c6dab0f9a5ec98cde
Tevis Bateman <A00954003@ad.bcit.ca>
2017-02-24 15:18:40 -0800

    big additions -- new test fonts, new pages, sticky nav script, footer, nav, head php sections

---
+++ v02/include/footer.php
@@ -0,0 +1,23 @@
+<footer>
+       
+   <div class="wrapper">
+
+       <nav class="footer-nav">
+
+           <ul>
+
+           </ul>
+
+       </nav>
+
+       <ul>
+           <li><a href="https://www.linkedin.com/in/tevis-bateman/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+           <li><a href="https://codepen.io/tevis-bateman/"><i class="fa fa-codepen" aria-hidden="true"></i></a></li>
+           <li><a href="https://github.com/regigold"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+       </ul>
+
+     <p>&copy; 2017 &mdash; Tevis Bateman Design</p>
+
+   </div>
+
+</footer> <!-- end footer -->
---
+++ v02/include/head.php
@@ -0,0 +1,16 @@
+<head>
+   
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <title>Tevis Bateman</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <!-- font-awesome -->
+    <link rel="stylesheet" text="text/css" href="styles/styles.css">
+    
+    <!-- style page -->
+    <!--[if lt IE 9]>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+    
+</head>
