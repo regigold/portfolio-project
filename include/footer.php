4b9c4e34d306f8a789892a94af1b48be75259006
Tevis Bateman <A00954003@ad.bcit.ca>
2017-02-24 17:19:35 -0800

    style changes made to nav -- index.php added

--- include/footer.php
+++ include/footer.php
@@ -11,9 +11,9 @@
        </nav>
 
        <ul>
-           <li><a href="https://www.linkedin.com/in/tevis-bateman/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-           <li><a href="https://codepen.io/tevis-bateman/"><i class="fa fa-codepen" aria-hidden="true"></i></a></li>
-           <li><a href="https://github.com/regigold"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+           <li><a href="https://www.linkedin.com/in/tevis-bateman/" target='_blank'><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+           <li><a href="https://codepen.io/tevis-bateman/" target='_blank'><i class="fa fa-codepen" aria-hidden="true"></i></a></li>
+           <li><a href="https://github.com/regigold" target='_blank'><i class="fa fa-github" aria-hidden="true"></i></a></li>
        </ul>
 
      <p>&copy; 2017 &mdash; Tevis Bateman Design</p>
