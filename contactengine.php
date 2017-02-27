7f28ca3c0ced2c853521d11e5257f4b275b65ee0
Tevis Bateman <tbateman88@gmail.com>
2017-02-27 02:43:46 -0800

    contact thank-you page styled

--- contactengine.php
+++ contactengine.php
@@ -1,8 +1,8 @@
 <?php
 
 $EmailFrom = "chriscoyier@gmail.com";
-$EmailTo = "CHANGE-THIS@YOUR-DOMAIN.com";
-$Subject = "Nice & Simple Contact Form by CSS-Tricks";
+$EmailTo = "hello@tevisbateman.com";
+$Subject = "Email from Tevisbateman.com";
 $Name = Trim(stripslashes($_POST['Name'])); 
 $Tel = Trim(stripslashes($_POST['Tel'])); 
 $Email = Trim(stripslashes($_POST['Email'])); 
--- contactthanks.php
+++ contactthanks.php
@@ -1,35 +1,25 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once 'include/head.php'; // include head ?>
 
-<head>
-	<title>A Nice & Simple Contact Form</title>
-	
-	<link rel="stylesheet" type="text/css" href="style.css" />
-</head>
-
-<body>
-
-	<div id="page-wrap">
-
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
-		
-			
-		<h1>Your message has been sent!</h1><br />
-		
-		<p><a href="index.html">Back to Contact Form</a></p>
-	
-	</div>
-	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-	</script>
-	<script type="text/javascript">
-	_uacct = "UA-68528-29";
-	urchinTracker();
-	</script>
-
-</body>
+    <body id="contact" class="thanks">
+        <header>
+            <div class="wrapper">
+                <?php include_once 'include/nav.php'; // include nav ?>
+        </header>
+        <!-- end header -->
+        <main>
+            <div class="wrapper">
+                <section class="intro container">
+                    <h3>Thanks, your message has been sent!</h3> 
+                </section>
+                <!-- end bio-intro container -->
+            </div>    
+        </main>
+        <!-- end main -->
+        <?php include_once 'include/footer.php'; // include footer ?>
+            <script src="scripts/jquery-3.1.1.min.js" type="text/javascript"></script>
+            <script src="scripts/hide-mobile-menus.js" type="text/javascript"></script>
+    </body>
 
 </html>
